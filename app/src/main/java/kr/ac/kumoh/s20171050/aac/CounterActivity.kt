852de9a7aabbf9b7937df8de76f1e65068de1f61
jeong3bin <jn8121@naver.com>
2020-08-10 13:26:01 +0900

    with Viewmodel

---
+++ app/src/main/java/kr/ac/kumoh/s20171050/aac/CounterActivity.kt
@@ -0,0 +1,38 @@
+package kr.ac.kumoh.s20171050.aac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import kr.ac.kumoh.s20171050.aac.databinding.ActivityCounterBinding
+
+class CounterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_counter)
+
+        val viewmodel: CounterViewmodel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(CounterViewmodel::class.java)
+
+        val binding: ActivityCounterBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_counter)
+
+        binding.countText.text = viewmodel.counter.toString()
+
+        binding.fabAdd.setOnClickListener {
+            viewmodel.counter++
+            binding.countText.text = viewmodel.counter.toString()
+        }
+
+
+        binding.fabRemove.setOnClickListener {
+            viewmodel.counter--
+            binding.countText.text = viewmodel.counter.toString()
+        }
+
+    }
+}
---
+++ app/src/main/java/kr/ac/kumoh/s20171050/aac/CounterViewmodel.kt
@@ -0,0 +1,9 @@
+package kr.ac.kumoh.s20171050.aac
+
+import androidx.lifecycle.ViewModel
+
+class CounterViewmodel : ViewModel() {
+
+    var counter: Int = 0
+
+}
